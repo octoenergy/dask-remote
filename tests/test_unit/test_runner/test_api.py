22af639dde4cec0041650fc07ddb6f59e595b9de
Igor Gotlibovych <igor.gotlibovych@gmail.com>
2020-01-14 15:47:45 +0000

    Return verbatim message from proxy through API

--- tests/test_unit/test_runner/test_api.py
+++ tests/test_unit/test_runner/test_api.py
@@ -18,7 +18,7 @@ def test_status(client):
 def test_scale(client):
     response = client.post("/scale/42")
     assert response.status_code == 200
-    assert response.json() == {"message": "Scaling to 42 workers"}
+    assert response.json() == {"message": "scale(42)"}
 
     response = client.get("/scale")
     assert response.json() == {"message": "42"}

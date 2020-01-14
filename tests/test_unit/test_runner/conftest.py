e00a71d1719a2faa29b999df068936c8895a756a
Igor Gotlibovych <igor.gotlibovych@gmail.com>
2020-01-14 11:23:08 +0000

    Instantiate Pipes with the ClusterProcess to simplify usage

--- tests/test_unit/test_runner/conftest.py
+++ tests/test_unit/test_runner/conftest.py
@@ -6,16 +6,6 @@ from dask_remote.runner.api import cluster_api
 from dask_remote.runner.cluster_process import ClusterProcess, ClusterProcessProxy
 
 
-@pytest.fixture
-def cmd_pipe():
-    return Pipe()
-
-
-@pytest.fixture()
-def result_pipe():
-    return Pipe()
-
-
 class PingCluster:
     def __init__(self, n=0):
         self.n = n
@@ -49,20 +39,28 @@ class PingCluster:
 
 
 @pytest.fixture
-def cluster_process(cmd_pipe, result_pipe):
-    cluster_process = ClusterProcess(
-        cmd_conn=cmd_pipe[1], result_conn=result_pipe[0], cluster_cls=PingCluster
-    )
-    cluster_process.start()
-    yield cluster_process
-    cluster_process.terminate()
-    cluster_process.join()
+def cmd_pipe():
+    return Pipe()
+
+
+@pytest.fixture()
+def result_pipe():
+    return Pipe()
 
 
 @pytest.fixture
 def cluster_process_proxy(cmd_pipe, result_pipe):
     cluster_process_proxy = ClusterProcessProxy(cmd_conn=cmd_pipe[0], result_conn=result_pipe[1])
     return cluster_process_proxy
+
+
+@pytest.fixture
+def cluster_process(cmd_pipe, result_pipe):
+    cluster_process = ClusterProcess(cluster_cls=PingCluster)
+    cluster_process.start()
+    yield cluster_process
+    cluster_process.terminate()
+    cluster_process.join()
 
 
 @pytest.fixture
@@ -76,6 +74,6 @@ def api_address(api_port):
 
 
 @pytest.fixture
-def api_app(cluster_process_proxy, cluster_process):
+def api_app(cluster_process):
     fastapi_kwargs = {}
-    return cluster_api(cluster_process_proxy, fastapi_kwargs=fastapi_kwargs)
+    return cluster_api(cluster_process.proxy, fastapi_kwargs=fastapi_kwargs)
--- tests/test_unit/test_runner/test_cluster_process.py
+++ tests/test_unit/test_runner/test_cluster_process.py
@@ -5,50 +5,6 @@ from dask_remote.runner.cluster_process import ClusterProcess
 from .conftest import PingCluster
 
 
-class TestClusterProcess:
-    @pytest.mark.parametrize("n", [0, 42])
-    def test_cluster_kwargs(self, n, cmd_pipe, result_pipe):
-        cluster_process = ClusterProcess(
-            cmd_conn=cmd_pipe[1],
-            result_conn=result_pipe[0],
-            cluster_cls=PingCluster,
-            cluster_kwargs={"n": n},
-        )
-        cluster_process.start()
-
-        cmd = {"attribute": "scheduler_info"}
-        cmd_pipe[0].send(cmd)
-        info = result_pipe[1].recv()
-
-        assert len(info["workers"]) == n
-
-        cluster_process.terminate()
-        cluster_process.join()
-
-    def test_attribute(self, cluster_process, cmd_pipe, result_pipe):
-        cmd = {"attribute": "attribute"}
-        cmd_pipe[0].send(cmd)
-        result = result_pipe[1].recv()
-
-        assert result == "attribute"
-
-    @pytest.mark.parametrize(
-        "cmd", [{"method": "method", "args": [42]}, {"method": "method", "kwargs": {"n": 42}}]
-    )
-    def test_method(self, cluster_process, cmd_pipe, result_pipe, cmd):
-        cmd_pipe[0].send(cmd)
-        result = result_pipe[1].recv()
-
-        assert result == "method(42)"
-
-    def test_returns_error(self, cluster_process, cmd_pipe, result_pipe):
-        cmd = {"attribute": "not_an_attribute"}
-        cmd_pipe[0].send(cmd)
-        result = result_pipe[1].recv()
-
-        assert isinstance(result, AttributeError)
-
-
 class TestClusterProcessProxy:
     @pytest.mark.parametrize("attr", ["scheduler_address"])
     def test_cluster_attribute(self, cluster_process_proxy, cmd_pipe, result_pipe, attr):
@@ -79,13 +35,57 @@ class TestClusterProcessProxy:
             _ = cluster_process_proxy.not_a_method()
 
 
+class TestClusterProcess:
+    @pytest.mark.parametrize("n", [0, 42])
+    def test_cluster_kwargs(self, n):
+        cluster_process = ClusterProcess(cluster_cls=PingCluster, cluster_kwargs={"n": n})
+        cluster_process.start()
+
+        cmd = {"attribute": "scheduler_info"}
+        cluster_process._cmd_pipe[0].send(cmd)
+        info = cluster_process._result_pipe[1].recv()
+
+        assert len(info["workers"]) == n
+
+        cluster_process.terminate()
+        cluster_process.join()
+
+    def test_attribute(self, cluster_process):
+        cmd = {"attribute": "attribute"}
+        cluster_process._cmd_pipe[0].send(cmd)
+        result = cluster_process._result_pipe[1].recv()
+
+        assert result == "attribute"
+
+    @pytest.mark.parametrize(
+        "cmd", [{"method": "method", "args": [42]}, {"method": "method", "kwargs": {"n": 42}}]
+    )
+    def test_method(self, cluster_process, cmd):
+        cluster_process._cmd_pipe[0].send(cmd)
+        result = cluster_process._result_pipe[1].recv()
+
+        assert result == "method(42)"
+
+    def test_returns_error(self, cluster_process):
+        cmd = {"attribute": "not_an_attribute"}
+        cluster_process._cmd_pipe[0].send(cmd)
+        result = cluster_process._result_pipe[1].recv()
+
+        assert isinstance(result, AttributeError)
+
+    def test_proxy(self, cluster_process):
+        proxy = cluster_process.proxy
+        assert proxy.cmd_conn is cluster_process._cmd_pipe[0]
+        assert proxy.result_conn is cluster_process._result_pipe[1]
+
+
 class TestIntegration:
-    def test_attribute(self, cluster_process, cluster_process_proxy):
-        assert cluster_process_proxy.scheduler_address == "scheduler_address"
+    def test_attribute(self, cluster_process):
+        assert cluster_process.proxy.scheduler_address == "scheduler_address"
 
     def test_method(self, cluster_process, cluster_process_proxy):
-        assert cluster_process_proxy.scale(42) == f"scale(42)"
+        assert cluster_process.proxy.scale(42) == f"scale(42)"
 
     def test_new_attribute(self, cluster_process, cluster_process_proxy):
         """Test attribute that is valid, but not part of the original cluster class."""
-        assert cluster_process_proxy.num_workers == 0
+        assert cluster_process.proxy.num_workers == 0
